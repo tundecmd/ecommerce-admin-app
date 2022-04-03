3c69a55358447690a34f70a80383093057dadad5
Adepoju Lateef <ademustexcel@gmail.com>
2022-04-03 20:23:21 +0100

    end 0f 31 - User ogin and Product Page

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,6 @@ import rootReducer from '../reducers';
 import { composeWithDevTools } from '@redux-devtools/extension';
 
 
-
 const store = createStore(rootReducer, composeWithDevTools(
   applyMiddleware(thunk)
 ));
